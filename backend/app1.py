b974d1e52afd5065a73e8f815014baa4bc101935
ujineugene <amyeugene7lee@gmail.com>
2025-03-26 14:07:19 +0900

    리캡라우트추가

--- backend/app1.py
+++ backend/app1.py
@@ -44,16 +44,19 @@ app.config['DEBUG'] = DEBUG
 # 그 이후에 라우트 모듈을 import합니다.
 from routes.auth import auth_bp
 from routes.diary import diary_bp
-from routes.letter import letter_bp
+#from routes.letter import letter_bp
 from scheduler.scheduler import init_scheduler
-
+from routes.recaps import recaps_bp
 # 블루프린트 등록
 app.register_blueprint(auth_bp)
 app.register_blueprint(diary_bp)
-app.register_blueprint(letter_bp)
+#app.register_blueprint(letter_bp)
+app.register_blueprint(recaps_bp)
 
 # 스케줄러 초기화
 init_scheduler()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=DEBUG)
+
+
